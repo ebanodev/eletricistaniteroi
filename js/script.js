ddac23a78b2f587df13704cc9c579cdf3ec0f54a
ebano.dev <ebanoassumpcao@gmail.com>
2021-03-10 20:51:06 -0300

    menu mobile-first ok

--- js/script.js
+++ js/script.js
@@ -1,55 +1,4 @@
 
-//MENU ACENDENDO
-function brightMenu() {
-	
-	
-	var brightMenu = document.querySelector("ul.menu-items");
-	brightMenu.classList.toggle('lightOn');
-}
-
-function changecolor(){
-//pega o conteúdo texto do objeto .fancy e separa cada caracter em um array com a função split.
-
-var brightBx = document.getElementById("brightBx");
-brightBx.classList.add('changecolor');
-
-
-setTimeout(function(){brightMenu();}, 1000);
-
-/* const text = document.querySelector(".changecolor"); 
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = ""; //para tirar a duplicação que ocorre no textContent após o laço de adição da tag span pra cada letra
-
-
-//laço para adição da tag span para cada letra da constante text
-for (let i = 0; i < splitText.length; i++) 
-{ text.innerHTML += "<span>" + splitText[i] + "</span>";   }
-
-//Setando os intervalos para a animação executando a função da animação que adiciona a classe de efeito a cada tag span.
-
-let char = 0;
-let timer = setInterval(onTick, 5);
-
-function onTick() {
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add('color');
-    char++
-    //para fazer o loop parar
-    if (char === splitText.length) {
-        complete();
-        return;
-    }
-    function complete()
-    {
-        clearInterval(timer);
-        timer = null;
-		
-    }
-} */
-}
-
-
 //FANCY
 
 function fancy(){
@@ -84,7 +33,6 @@ function onTick() {
         clearInterval(timer);
         timer = null;
 		
-		setTimeout(function(){changecolor();}, 800);
     }
 }
 }
@@ -107,12 +55,12 @@ const callback = (entries, observer) => {
         if (entry.isIntersecting == false)              
         {
 			acenderMenu.classList.add('acender');
-		portifolio.innerHTML = "Ébano Assumpção";
+		portifolio.innerHTML = "Eletricista Niterói";
         }
         else
         {
 			
-			portifolio.innerHTML = "Portifolio";
+			portifolio.innerHTML = "Assumpção";
 			acenderMenu.classList.remove('acender');
         };
 
